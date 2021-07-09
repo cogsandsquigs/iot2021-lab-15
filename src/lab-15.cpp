0710df0bf5f0b3653e75b78a5a27a3406389f162
Ian Pratt <ianjdpratt@gmail.com>
2021-07-08 20:50:41 -0500

    first commit

---
+++ src/lab-15.cpp
@@ -0,0 +1,75 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/home/ianp/Documents/iot2021/labs/lab-15/src/lab-15.ino"
+void setup();
+void loop();
+void T1(void *args);
+void T2(void *args);
+void T3(void *args);
+#line 1 "/home/ianp/Documents/iot2021/labs/lab-15/src/lab-15.ino"
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+
+os_thread_t t1;
+os_thread_t t2;
+os_thread_t t3;
+
+//os_mutex_t serial_mutex;
+
+int inc = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  //os_mutex_create(&serial_mutex);
+  os_thread_create(&t1, "t1", OS_THREAD_PRIORITY_DEFAULT, T1, NULL, 1024);
+  os_thread_create(&t2, "t2", OS_THREAD_PRIORITY_DEFAULT, T2, NULL, 1024);
+  os_thread_create(&t3, "t3", OS_THREAD_PRIORITY_DEFAULT, T3, NULL, 1024);
+
+  pinMode(D7, OUTPUT);
+}
+
+void loop()
+{
+}
+
+void T1(void *args)
+{
+  while (1)
+  {
+    WITH_LOCK(Serial)
+    {
+      Serial.println("random value: " + String(random(100)));
+    }
+    delay(1000);
+  }
+}
+
+void T2(void *args)
+{
+
+  while (1)
+  {
+    WITH_LOCK(Serial)
+    {
+      Serial.println("incremented value: " + String(inc));
+    }
+    delay(500);
+    inc += 1;
+  }
+}
+
+void T3(void *args)
+{
+  while (1)
+  {
+    digitalWrite(D7, HIGH);
+    delay(250);
+    digitalWrite(D7, LOW);
+    delay(250);
+  }
+}
---
+++ src/lab-15.ino
@@ -0,0 +1,60 @@
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+
+os_thread_t t1;
+os_thread_t t2;
+os_thread_t t3;
+
+int inc = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  os_thread_create(&t1, "t1", OS_THREAD_PRIORITY_DEFAULT, T1, NULL, 1024);
+  os_thread_create(&t2, "t2", OS_THREAD_PRIORITY_DEFAULT, T2, NULL, 1024);
+  os_thread_create(&t3, "t3", OS_THREAD_PRIORITY_DEFAULT, T3, NULL, 1024);
+
+  pinMode(D7, OUTPUT);
+}
+
+void loop()
+{
+}
+
+void T1(void *args)
+{
+  while (1)
+  {
+    WITH_LOCK(Serial)
+    {
+      Serial.println("random value: " + String(random(100)));
+    }
+    delay(1000);
+  }
+}
+
+void T2(void *args)
+{
+
+  while (1)
+  {
+    WITH_LOCK(Serial)
+    {
+      Serial.println("incremented value: " + String(inc));
+    }
+    delay(500);
+    inc += 1;
+  }
+}
+
+void T3(void *args)
+{
+  while (1)
+  {
+    digitalWrite(D7, HIGH);
+    delay(250);
+    digitalWrite(D7, LOW);
+    delay(250);
+  }
+}
